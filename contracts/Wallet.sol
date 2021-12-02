b1c0513c345b074c4def02a028d3664e1433194c
ali-05-ahmed <extricateshareware@gmail.com>
2021-12-02 15:13:25 +0500

    wallet updated

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -8,7 +8,14 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract Wallet is Pausable{
     uint256 private id;
 
+    struct owner{
+        address nftContract;
+        uint256 id;
+    }
+
     mapping(address=> mapping(uint256=>bool)) public NFTexist;
+    mapping(address =>mapping(uint256=>address)) private ownerOf;
+    mapping(address=> uint256)public balance;
 
     function update() public whenNotPaused returns(uint256){
         id++;
@@ -26,8 +33,9 @@ contract Wallet is Pausable{
    
   //  require(msg.value == listingPrice, "Price must be equal to listing price");
     NFTexist[nftContract][tokenId] = true;
- 
-
+    
+    balance[_msgSender()] ++; 
+    ownerOf[nftContract][tokenId] = _msgSender();
     IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
 
   }
@@ -38,18 +46,19 @@ contract Wallet is Pausable{
     address to
   ) public payable whenNotPaused {
     require(NFTexist[nftContract][tokenId] == true, "NFT does not Exist on the market");
-   
+    
+    balance[_msgSender()] --;
+    ownerOf[nftContract][tokenId] = address(0);
   //  require(msg.value == listingPrice, "Price must be equal to listing price");
-   
- 
-
     IERC721(nftContract).transferFrom(address(this),to, tokenId);
 
      NFTexist[nftContract][tokenId] = false;
 
   }
 
-
+  function _ownerOf(address nftContract,uint256 tokenId) public view returns(address){
+    return ownerOf[nftContract][tokenId];
+  }
     /*
      * @dev Returns to normal state.
      *
